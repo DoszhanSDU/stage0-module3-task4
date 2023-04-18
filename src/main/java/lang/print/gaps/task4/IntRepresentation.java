31af2c5dfb3cf97f550d1ff09f78df6e814eba7e
DosyaSDU <doszhan_turar@mail.ru>
2023-04-18 15:05:28 +0600

    last

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,9 +2,9 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
-        System.out.println((char)99);
-        System.out.println((char)104);
-        System.out.println((char)97);
-        System.out.println((char)114);
+        System.out.print((char)99);
+        System.out.print((char)104);
+        System.out.print((char)97);
+        System.out.print((char)114);
     }
 }
